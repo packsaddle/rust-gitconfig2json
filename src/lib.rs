6f5fcfe97893de61cc1826cc4d7197c6e252e161
Sanemat <o.gata.ken@gmail.com>
2017-11-16 01:57:42 +0900

    refactor(gitconfig): extract to gitconfig

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,15 @@
 extern crate serde_json;
 
 use std::error::Error;
-use std::iter::FromIterator;
 mod gitconfig;
 use gitconfig::{Value, Map};
 use gitconfig::map::Entry;
 
 pub fn run(message: &str) -> Result<String, Box<Error>> {
-    let git_configs = Vec::from_iter(message.split("\0").map(String::from));
-    let mut map = Map::new();
-
-    for git_config in &git_configs {
-        if git_config.is_empty() {
-            continue;
-        }
-        let (keys, value) = split_once(git_config);
-        if keys.len() == 0 {
-            continue;
-        }
-        let split_keys = Vec::from_iter(keys.split(".").map(String::from));
-        match split_keys.len() {
-            1 => {
-                map.insert(split_keys[0].to_owned(), Value::String(value.to_owned()));
-                ()
-            }
-            2 => {
-                // TODO: split_keys[0].clone() why clone??
-                match map.entry(split_keys[0].clone()) {
-                    Entry::Occupied(mut occupied) => {
-                        occupied.get_mut().as_object_mut().unwrap().insert(
-                            split_keys[1]
-                                .to_owned(),
-                            Value::String(
-                                value.to_owned(),
-                            ),
-                        );
-                        ()
-                    }
-                    Entry::Vacant(vacant) => {
-                        let mut internal = Map::new();
-                        internal.insert(split_keys[1].to_owned(), Value::String(value.to_owned()));
-                        vacant.insert(Value::Object(internal));
-                        ()
-                    }
-                }
-            }
-            n if n >= 3 => {
-                // TODO: split_keys[0].clone() why clone??
-                match map.entry(split_keys[0].clone()) {
-                    Entry::Occupied(mut occupied) => {
-                        match occupied.get_mut().as_object_mut().unwrap().entry(
-                            split_keys
-                                [1..n - 1]
-                                .join("."),
-                        ) {
-                            Entry::Occupied(mut occupied2) => {
-                                occupied2.get_mut().as_object_mut().unwrap().insert(
-                                    split_keys[n - 1]
-                                        .to_owned(),
-                                    Value::String(
-                                        value.to_owned(),
-                                    ),
-                                );
-                                ()
-                            }
-                            Entry::Vacant(vacant2) => {
-                                let mut internal = Map::new();
-                                internal.insert(
-                                    split_keys[n - 1].to_owned(),
-                                    Value::String(value.to_owned()),
-                                );
-                                vacant2.insert(Value::Object(internal));
-                                ()
-                            }
-                        }
-                    }
-                    Entry::Vacant(vacant) => {
-                        let mut internal = Map::new();
-                        internal.insert(
-                            split_keys[n - 1].to_owned(),
-                            Value::String(value.to_owned()),
-                        );
-                        let mut external = Map::new();
-                        external.insert(split_keys[1..n - 1].join("."), Value::Object(internal));
-                        vacant.insert(Value::Object(external));
-                        ()
-                    }
-                }
-            }
-            _ => return Err(From::from("unexpected something happens.".to_owned())),
-        }
+    match serde_json::to_string(&convert(message.parse()?)) {
+        Ok(t) => Ok(t),
+        Err(e) => Err(Box::new(e)),
     }
-
-    Ok(serde_json::to_string(&convert(Value::Object(map))).unwrap())
-}
-
-fn split_once(in_string: &str) -> (&str, &str) {
-    let mut splitter = in_string.splitn(2, "\n");
-    let first = splitter.next().unwrap();
-    let second = splitter.next().unwrap();
-    (first, second)
 }
 
 fn convert(git_config: gitconfig::Value) -> serde_json::Value {
